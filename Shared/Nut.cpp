a28165e0c7d44d691f1a008e696d3014bd80c9f4
Jinouga47 <b7010863@my.shu.ac.uk>
2019-01-21 16:52:52 +0000

    New features

--- Shared/Nut.cpp
+++ Shared/Nut.cpp
@@ -3,7 +3,13 @@ RandomNumberGenerator Nut::rng_ = RandomNumberGenerator();
 
 Nut::Nut() : MoveableGridItem(NUT, rng_.get_random_value(SIZE), rng_.get_random_value(SIZE)), collected_(false)
 {
+	if (get_x() == 1 || get_x() == SIZE) {
+		reset();
+	}
 
+	if (get_y() == 1 || get_y() == SIZE) {
+		reset();
+	}
 }
 
 
@@ -46,19 +52,28 @@ bool Nut::move(int mouse_x, int mouse_y)
 	int nut_x_ = (get_x() - mouse_x);
 	int nut_y_ = (get_y() - mouse_y);
 
-	if (mouse_near(nut_x_, nut_y_)) {
-		if (boundary_check(nut_x_ * 2, nut_y_ * 2)) {
+	if (mouse_near(nut_x_, nut_y_))
+	{
+		if (boundary_check(nut_x_ * 2, nut_y_ * 2))
+		{
+			int nut_dx_ = nut_x_;
+			int nut_dy_ = nut_y_;
 			// update nut coordinates if move is possible
-			if (((get_x() + nut_dx_) >= 2) && ((get_x() + nut_dx_) <= SIZE - 1) && ((get_y() + nut_dy_) >= 2) && ((get_y() + nut_dy_) <= SIZE - 1))
+			if (((get_x() + nut_dx_ * 2) >= 2) && ((get_x() + nut_dx_ * 2) <= SIZE - 1) && ((get_y() + nut_dy_ * 2) >= 2) && ((get_y() + nut_dy_ * 2) <= SIZE - 1))
 			{
 				update_position(nut_dx_, nut_dy_);
+				return true;
 			}
 		}
-		
-
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
 		return true;
 	}
-	
 }
 
 void Nut::AssignCollected(bool collected) {
